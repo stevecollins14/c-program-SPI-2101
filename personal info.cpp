c6d3798ddbe36b5d71a800ad79b2042bf3915dc6
stevecollins14 <viestacollins@gmail.com>
2025-09-18 11:16:30 +0300

    Add files via upload

---
+++ personal info.cpp
@@ -0,0 +1,45 @@
+//simple c program
+/*
+Name: Steve Collins Munene
+Reg: CT100/G/26243/25
+Description: Personal Information
+*/
+
+
+#include<stdio.h>//
+
+//main function
+int main(){
+
+int Height ;
+printf("Enter Your Height");
+    
+scanf("%d",&Height);
+    
+printf("Your Height is %d\n",Height);
+
+int Age ;
+printf(" Enter Your Age");
+    
+scanf("%d",&Age);
+    
+printf("Your Age is %dyears\n",Age );
+
+ 
+ int Weight ;
+printf(" Enter Your Weight");
+    
+scanf("%d",&Weight);
+    
+printf("Your Weight is %dKg\n",Weight );
+    
+    
+     
+ int contact ;
+printf(" Enter Your Contact");
+
+scanf("%d",&contact);
+    
+printf("Your contact is %d\n",contact);
+return 0;
+}
---
+++ units for it.cpp
@@ -0,0 +1,20 @@
+//simple c program(Units for Information Technology for first year semester one)
+/*
+Name: Steve Collins Munene
+Reg: CT100/G/26243/25
+Description: Units For Information Technology for first year semester one
+*/
+
+#include<stdio.h>//
+
+int main (){
+    printf("SPH 2107    Analogue Electronics \n");
+    printf("SPI 2102    Computer System and Organisation \n");
+    printf("UCU 2105    Fundamentals of ICT \n");
+    printf("SPI 2103    Introduction to Computer Application \n");
+    printf("SPI 2120    Essentials of Accounting \n");
+    printf("UCU 2101    Communication Skills \n");
+    printf("SPI 2101    Introduction to Programming and Algorithms \n");
+    printf("SPM 2104    Mathematics For Science /n");
+return 0;
+}
